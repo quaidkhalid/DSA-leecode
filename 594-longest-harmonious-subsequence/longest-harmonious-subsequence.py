dd8e02989f0971453ae060331541c66781d31462
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-09-05 22:28:29 +0500

    Time: 227 ms (86.90%) | Memory: 18.2 MB (89.00%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -1,19 +1,5 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        # hsh = {}
-        # for i in nums:
-        #     if i in hsh:
-        #         hsh[i] =+ 1
-        #     else:
-        #         hsh[i] = 1
-        # ans = 0
-        # for i in hsh:
-        #     x = hsh[i]
-        #     if i+1 in hsh:
-        #         y = hsh[i+1]
-        #         z = x + y
-        #         if z > ans:
-        #             ans = z
         hsh = {}
         for i in nums:
             if i in hsh:
@@ -25,7 +11,21 @@ class Solution:
             x = hsh[i]
             if i+1 in hsh:
                 y = hsh[i+1]
-                z = x+y
-                if z > ans :
+                z = x + y
+                if z > ans:
                     ans = z
+        # hsh = {}
+        # for i in nums:
+        #     if i in hsh:
+        #         hsh[i] += 1
+        #     else:
+        #         hsh[i] = 1
+        # ans = 0
+        # for i in hsh:
+        #     x = hsh[i]
+        #     if i+1 in hsh:
+        #         y = hsh[i+1]
+        #         z = x+y
+        #         if z > ans :
+        #             ans = z
         return ans
