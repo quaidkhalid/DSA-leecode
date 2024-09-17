bec4420aefc640d029547bad70170c451b6e8d25
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-09-17 14:16:54 +0500

    Time: 187 ms (6.39%) | Memory: 18 MB (20.97%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,35 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # res,count=0,0
-        # for i in nums:
-        #     if count==0:
-        #         res = i
-        #     if res ==i:
-        #         count+=1
-        #     else:
-        #         count-=1
-        # return res
-        # x = []
-        # for i in nums:
-        #     if i in x:
-        #         count += 1
-        #     else:
-        #         count[i] = 1
-        # return count
-        # O(nlogn) complexity by apna college
 
-        nums.sort()
-        freq = 1
-        ans = nums[0]
-        n = len(nums)
-        for i in range(1, n):
-            if nums[i] == nums[i-1]:
-                freq += 1
+        # O(nlogn) complexity by apna college lec 11, DSA series
+
+        # nums.sort()
+        # freq = 1
+        # ans = nums[0]
+        # n = len(nums)
+        # for i in range(1, n):
+        #     if nums[i] == nums[i-1]:
+        #         freq += 1
                 
-            else:
-                freq = 1
-                ans = nums[i]
+        #     else:
+        #         freq = 1
+        #         ans = nums[i]
 
-            if freq > (n//2):
-                return ans
+        #     if freq > (n//2):
+        #         return ans
+        # return ans
+
+        #optimized approch Moore's voting algo
+        freq = 0
+        ans = 0
+        for i in range(len(nums)):
+            if freq == 0:
+                ans = nums[i]
+            if ans == nums[i]:
+                freq += 1
+            else:
+                freq -= 1
         return ans
