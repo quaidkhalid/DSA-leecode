99f903f2f2a3e1a8b1f43df0cea4e3da99a95f4e
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-10-11 07:19:00 +0500

    Time: 179 ms (23.31%) | Memory: 18.1 MB (31.82%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,32 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-
-        # O(nlogn) complexity by apna college lec 11, DSA series
-
-        # nums.sort()
-        # freq = 1
-        # ans = nums[0]
-        # n = len(nums)
-        # for i in range(1, n):
-        #     if nums[i] == nums[i-1]:
-        #         freq += 1
-                
-        #     else:
-        #         freq = 1
-        #         ans = nums[i]
-
-        #     if freq > (n//2):
-        #         return ans
-        # return ans
-
-        #optimized approch Moore's voting algo
-        freq = 0
-        ans = 0
-        for i in range(len(nums)):
-            if freq == 0:
-                ans = nums[i]
-            if ans == nums[i]:
+        nums.sort()
+        freq = 1 
+        ans = nums[0]
+        n = len(nums)
+        for i in range(1,n):
+            if nums[i] == nums[i-1]:
                 freq += 1
             else:
-                freq -= 1
+                freq = 1
+                ans = nums[i]
+
+            if freq > (n//2):
+                return ans
         return ans
