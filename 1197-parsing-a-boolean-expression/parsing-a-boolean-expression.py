0279d008d9731985ece4f5ef638fc66118af7bd6
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-10-20 20:58:54 +0500

    Time: 8 ms (100.00%) | Memory: 16.8 MB (50.60%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
@@ -1,20 +1,15 @@
 class Solution:
     def solveOp(self, op, values):
-        """Evaluates the logical operation based on the operator and values."""
         if op == '!':
-            # Negation: return False if value is True, else return True
             return 'f' if values[0] == 't' else 't'
         
         if op == '&':
-            # And: return False if any value is False, else return True
             return 'f' if 'f' in values else 't'
         
         if op == '|':
-            # Or: return True if any value is True, else return False
             return 't' if 't' in values else 'f'
     
     def parseBoolExpr(self, s: str) -> bool:
-        """Parses the boolean expression and returns the result."""
         stack = []
         for char in s:
 
@@ -23,14 +18,14 @@ class Solution:
 
             elif char == ')':
                 values = []
-
-                # Gather all values inside the parentheses
                 while stack[-1] != '(':
                     values.append(stack.pop())
                 stack.pop()  # Remove '('
                 op = stack.pop()  # Remove the operator
+
                 stack.append(self.solveOp(op, values))
+
             else:
                 stack.append(char)
-                
+
         return stack[-1] == 't'
