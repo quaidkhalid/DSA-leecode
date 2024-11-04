d3eae301911de673abaa54e85996c3de839fe818
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-11-04 08:32:39 +0500

    Time: 0 ms (100.00%) | Memory: 23.9 MB (89.24%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,28 +1,28 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         n = len(nums)
-        l , r = 0 , n-1
-        if n==1:
+        l , r = 0, n-1
+        if len(nums) == 1:
             return nums[0]
-        
-        while l <= r:
+
+        while l<=r:
             mid = r + (l-r) // 2
-            if mid == 0 and nums[0]!=nums[1]:
+            if mid == 0 and nums[0] != nums[1]:
                 return nums[mid]
             if mid == n-1 and nums[n-1]!=nums[n-2]:
                 return nums[mid]
-
-            if nums[mid-1] != nums[mid] and nums[mid] != nums[mid+1]:
+                
+            if nums[mid-1] != nums[mid] and nums[mid]!=nums[mid + 1]:
                 return nums[mid]
 
-            if mid % 2 == 0: # even numbers on both side of mid
+            if mid % 2 ==0: #even numbers on bs
                 if nums[mid-1] == nums[mid]:
-                    r = mid - 1
-                else:
+                    r = mid -1
+                else: 
                     l = mid + 1
-            
-            else: #odd numbers on both side of mid
-                if nums[mid-1] == nums[mid]:
-                    l = mid + 1
+
+            else:
+                if nums[mid-1]==nums[mid]:
+                    l = mid+1
                 else:
-                    r = mid - 1
+                    r = mid-1
