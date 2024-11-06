e23774d409d2b63b2b7f06b699f5f6aea523b4c6
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-11-06 18:14:53 +0500

    Time: 83 ms (19.09%) | Memory: 16.7 MB (23.35%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j] <= nums[j+1]:
+                    continue
+                else:
+                    if bin(nums[j]).count('1') == bin(nums[j+1]).count('1'):
+                        nums[j],nums[j+1]=nums[j+1],nums[j]
+                    else:
+                        return False
+        return True        
