59a032c2e2af596c7a00e73877d1ca393b351941
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2024-11-18 08:33:16 +0500

    Time: 106 ms (62.33%) | Memory: 26.3 MB (83.83%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -3,20 +3,75 @@ class Solution:
         n = len(temperatures)
         stack = []
         result = [0] * n
-        
-        # Traverse temperatures from right to left
-        for i in range(n - 1, -1, -1):
-            # Remove indices from stack while current temperature is greater or equal
+
+        for i in range(n-1, -1,-1):
             while stack and temperatures[i] >= temperatures[stack[-1]]:
                 stack.pop()
-            
-            # If stack is not empty, calculate the difference in days
-            if stack:
+
+            if not stack:
+                result[i] = 0
+            else:
                 result[i] = stack[-1] - i
-            
-            # Push current index to stack
+
             stack.append(i)
-        
+
         return result
+
+            
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(temperatures)
+        # stack = []
+        # result = [0] * n
+        
+        # # Traverse temperatures from right to left
+        # for i in range(n - 1, -1, -1):
+        #     # Remove indices from stack while current temperature is greater or equal
+        #     while stack and temperatures[i] >= temperatures[stack[-1]]:
+        #         stack.pop()
+            
+        #     if not stack:
+        #         result[i] = 0
+        #     else:
+        #         result[i] = stack[-1] - i
+            
+        #     # Push current index to stack
+        #     stack.append(i)
+        
+        # return result
 
         
