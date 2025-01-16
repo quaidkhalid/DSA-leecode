299d930127d0cc23f30dfda9f278363baefc7054
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2025-01-16 22:34:58 +0500

    Time: 4 ms (58.38%) | Memory: 36.4 MB (60.69%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -2,16 +2,26 @@ class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         m = len(nums1)
         n = len(nums2)
-        map = {}
-        for num in nums1:
-            map[num] = map.get(num, 0) + n
-        for num in nums2:
-            map[num] = map.get(num,0) + m
+        # map = {}
+        # for num in nums1:
+        #     map[num] = map.get(num, 0) + n
+        # for num in nums2:
+        #     map[num] = map.get(num,0) + m
 
-        res = 0
-        for num in map:
-            if map[num] % 2 != 0:
-                res ^= num
-        return res
+        # res = 0
+        # for num in map:
+        #     if map[num] % 2 != 0:
+        #         res ^= num
+        # return res
+        XOR = 0
+        if m % 2 != 0:
+            for num in nums2:
+                XOR ^= num
+
+        if n % 2 != 0:
+            for num in nums1:
+                XOR ^= num
+
+        return XOR
 
         
