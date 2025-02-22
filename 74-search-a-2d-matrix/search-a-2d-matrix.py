ab3695e30aed43fcf6874c415e372322db8351bb
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2025-02-22 23:32:11 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (71.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,19 +1,32 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix:
-            return False
-            
-        m, n = len(matrix), len(matrix[0])
-        left, right = 0, m * n - 1
-
-        while left <= right:
-            mid = (left + right) // 2
-            mid_row, mid_col = mid//n, mid%n
-
-            if matrix[mid_row][mid_col] == target:
+    def searchInRow(self,matrix, target, row):
+        n = len(matrix[0])
+        st, end = 0, n-1
+        
+        while st <= end:
+            mid = st + (end-st)// 2
+            if target == matrix[row][mid]:
                 return True
-            elif matrix[mid_row][mid_col] < target:
-                left = mid + 1
+            elif target > matrix[row][mid]:
+                st = mid + 1
             else:
-                right = mid - 1
+                end = mid - 1
+
         return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix) 
+        n = len(matrix[0])
+        strow, endrow = 0, m-1
+        
+        while strow <= endrow:
+            midRow = strow + (endrow - strow) // 2
+            if target >= matrix[midRow][0] and target <= matrix[midRow][n-1]:
+                return self.searchInRow(matrix, target, midRow)
+
+            elif target >= matrix[midRow][n-1]:
+                strow = midRow + 1
+            else:
+                endrow = midRow - 1
+
+        return False
+        
