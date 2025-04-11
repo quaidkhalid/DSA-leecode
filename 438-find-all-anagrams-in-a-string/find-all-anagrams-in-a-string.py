be0caded9194544b21a7c47a6ce1f5cb672cd954
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2025-04-11 23:10:54 +0500

    Time: 37 ms (78.26%) | Memory: 18.6 MB (16.46%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        char_freqs, indices, len_p, len_s = defaultdict(int), [], len(p), len(s)
+        
+        # s cannot have p anangrams if len(p) > len(s)
+        if len_p > len_s:
+            return indices
+
+        # build map of character frequencies in p
+        for char in p:
+            char_freqs[char] += 1
+
+        # initial full pass over the window, except last element which we will pass over later
+        for i in range(len_p - 1):
+            if s[i] in char_freqs:
+                char_freqs[s[i]] -= 1
+            
+        # slide the window with stride 1, adding the value "sliding out" and subtracting the value "sliding in"
+        for i in range(-1, len_s - len_p + 1):
+            if i > -1 and s[i] in char_freqs:
+                char_freqs[s[i]] += 1
+            if i + len_p < len_s and s[i + len_p] in char_freqs: 
+                char_freqs[s[i + len_p]] -= 1
+                
+            # check whether we encountered an anagram by seeing if all frequencies add up to 0
+            if not any(char_freqs.values()): 
+                indices.append(i + 1)
+                
+        return indices
+
+        
