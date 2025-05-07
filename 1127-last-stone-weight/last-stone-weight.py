d7449a74370d5b511e4896e753d0c011a05a002d
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2025-05-07 21:56:33 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.06%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,15 +1,25 @@
 import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        for i in range(len(stones)):
-            stones[i] = stones[i] * -1
-        heapq.heapify(stones)
-        while len(stones) > 1:
-            x = heapq.heappop(stones)
-            y = heapq.heappop(stones)
+        # for i in range(len(stones)):
+        #     stones[i] = stones[i] * -1
+        # heapq.heapify(stones)
+        # while len(stones) > 1:
+        #     x = heapq.heappop(stones)
+        #     y = heapq.heappop(stones)
             
-            if y > x:
-                heapq.heappush(stones,x-y)
-        if len(stones) > 0:
-            return -stones[0]
-        return 0        
+        #     if y > x:
+        #         heapq.heappush(stones,x-y)
+        # if len(stones) > 0:
+        #     return -stones[0]
+        # return 0     
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            if abs(first) > abs(second):
+                heapq.heappush(stones, -(abs(first)-abs(second)))  
+
+        return -stones[0] if stones else 0   
