3c8ff9428038f942d434b2f9133181a7cb1d58a1
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2025-09-30 06:02:43 +0500

    Time: 1697 ms (27.92%) | Memory: 18.3 MB (87.04%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,9 +5,10 @@ class Solution:
         #         if nums[i] + nums[j] == target:
         #             return i,j
         # return 
-        my_dict={}
-        for i in range(len(nums)):
-            if nums[i] in my_dict:
-                return [my_dict[nums[i]],i]
-            else:
-                my_dict[target-nums[i]] = i
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i] + nums[j] == target:
+                    return i,j
+
+        return
