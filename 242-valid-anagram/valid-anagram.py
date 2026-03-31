719449a8a866cc03892fdabd9792ed7b12985b07
Quaid Khalid <133941589+quaidkhalid@users.noreply.github.com>
2026-03-31 09:37:00 -0500

    Time: 13 ms (50.21%) | Memory: 19.5 MB (60.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,15 +5,26 @@ class Solution:
         # return sorted_s == sorted_t
 
         #using Hash Map 
-        countS ,  countT = {} , {}
+        # countS ,  countT = {} , {}
+        # if len(s) != len(t):
+        #     return False
+            
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i], 0)
+        #     countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        # if countS != countT:
+        #     return False
+        # return True 
         if len(s) != len(t):
             return False
-            
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+        count = [0] * 26
+        for char in s:
+            count[ord(char) - ord('a')] += 1
+        print(count)
 
-        if countS != countT:
-            return False
-        return True 
-        
+        for char in t:
+            if count[ord(char) - ord('a')] == 0:
+                return False
+            count[ord(char) - ord('a')] -= 1
+        return True
